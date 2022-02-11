73bd0cbe3134a0ee2fe665d84c7b2cc052bf3f1f
Roberto Arcomano <bertolinux@gmail.com>
2022-02-11 22:10:00 +0100

    Changed to class and added a button

--- App.js
+++ App.js
@@ -6,140 +6,62 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
+import React, {Component} from 'react';
+import {Button, View, Text} from 'react-native';
 import { AnimatedGaugeProgress, GaugeProgress } from 'react-native-simple-gauge';
 
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+class App extends Component {
+	constructor(props) {
+	  super(props)
+	  this.state = {
+	    visible: false
+	  }
+		this.size = 200;
+		this.fill = 100;
+		this.width = 15;
+		this.cropDegree = 90;
+		this.textOffset = this.width;
+		this.textWidth = this.size - (this.textOffset*2);
+		this.textHeight = this.size*(1 - this.cropDegree/360) - (this.textOffset*2);
+		this.onPress = this.onPress.bind(this)
+	}
+	onPress() {
+		this.setState({visible: !this.state.visible})
+	}
+	render() {
+		const animation =
+				this.state.visible
+				? <AnimatedGaugeProgress
+	         size={200}
+	         width={15}
+	         fill={100}
+	         rotation={90}
+	         cropDegree={90}
+	         tintColor="#4682b4"
+	         delay={10}
+	         backgroundColor="#b0c4de"
+	         stroke={[2, 2]} //For a equaly dashed line
+	         strokeCap="circle" />
+	      : <View></View>
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+		const gauge =
+			<GaugeProgress
+	     size={this.size}
+	     width={this.width}
+	     fill={this.fill}
+	     cropDegree={this.cropDegree}>
+	     <View>
+	       <Text>hello</Text>
+	     </View>
+	   </GaugeProgress>
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+		return (
+			<View>
+			 {gauge}
+			 {animation}
+			 <Button title="Refresh" onPress={this.onPress}/>
+			</View>)
+	}
 };
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-	const size = 200;
-	const fill = 100;
-	const width = 15;
-	const cropDegree = 90;
-	const textOffset = width;
-	const textWidth = size - (textOffset*2);
-	const textHeight = size*(1 - cropDegree/360) - (textOffset*2);
-
-	return (
-		<View>
-		 <GaugeProgress
-       size={size}
-       width={width}
-       fill={fill}
-       cropDegree={cropDegree}>
-       <View style={styles.textView}>
-         <Text style={styles.text}>hello</Text>
-       </View>
-     </GaugeProgress>
-		 <AnimatedGaugeProgress
-           size={200}
-           width={15}
-           fill={100}
-           rotation={90}
-           cropDegree={90}
-           tintColor="#4682b4"
-           delay={0}
-           backgroundColor="#b0c4de"
-           stroke={[2, 2]} //For a equaly dashed line
-           strokeCap="circle" />
-		</View>)
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
