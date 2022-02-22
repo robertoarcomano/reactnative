17dfd27ce701d30e7e9795312ec660e00e417061
Roberto Arcomano <bertolinux@gmail.com>
2022-02-22 15:20:56 +0100

    It works. Start needs fixing

--- App.js
+++ App.js
@@ -1,102 +1,124 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React, {Component} from 'react';
-import {Button, View, Text} from 'react-native';
+import React, { useRef, useState } from "react";
+import { Button, DrawerLayoutAndroid, Text, StyleSheet, View } from "react-native";
 import { AnimatedGaugeProgress, GaugeProgress } from 'react-native-simple-gauge';
 import ProgressCircle from 'react-native-progress-circle'
 
-class App extends Component {
-	constructor(props) {
-	  super(props)
-	  this.state = {
-	    visible: false,
-	    perc: 0
-	  }
-		this.size = 200;
-		this.fill = 100;
-		this.width = 15;
-		this.cropDegree = 90;
-		this.textOffset = this.width;
-		this.textWidth = this.size - (this.textOffset*2);
-		this.textHeight = this.size*(1 - this.cropDegree/360) - (this.textOffset*2);
-		this.onPress = this.onPress.bind(this)
-		this.doSleep = this.doSleep.bind(this)
-		this.start = this.start.bind(this)
-	}
-	onPress() {
-		this.setState({visible: !this.state.visible})
-	}
-	sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-  doSleep(ms, callback) {
-    this.sleep(ms).then(callback())
-  }
-	async start() {
-		let i = 1
-		while (i++<100) {
-			await this.sleep(10)
-			this.setState({perc: i})
-		}
-	}
-	render() {
-		const animation =
-				this.state.visible
-				? <AnimatedGaugeProgress
-	         size={200}
-	         width={15}
-	         fill={100}
-	         rotation={90}
-	         cropDegree={90}
-	         tintColor="#4682b4"
-	         delay={10}
-	         backgroundColor="#b0c4de"
-	         stroke={[2, 2]} //For a equaly dashed line
-	         strokeCap="circle" />
-	      : <View></View>
+const progressCircle =
+	(perc,bgColor) =>
+		<ProgressCircle
+		  percent={perc}
+		  radius={50}
+		  borderWidth={8}
+		  color="#3399FF"
+		  shadowColor="#999"
+		  bgColor={bgColor}>
+		  <Text style={{ fontSize: 18 }}>{perc + "%"}</Text>
+		</ProgressCircle>
 
-		const gauge =
-			<GaugeProgress
-	     size={this.size}
-	     width={this.width}
-	     fill={this.fill}
-	     cropDegree={this.cropDegree}>
-	     <View>
-	       <Text>hello</Text>
-	     </View>
-	   </GaugeProgress>
+const gauge =
+	props =>
+		<GaugeProgress
+     size={props.size}
+     width={props.width}
+     fill={props.fill}
+     cropDegree={props.cropDegree}
+	   tintColor="#4682b4">
+     <View>
+       <Text>hello</Text>
+     </View>
+   </GaugeProgress>
 
-//        bgColor="#8acf89">
-		const redColor=255-2.5*this.state.perc
-		const greenColor=2.5*this.state.perc
-		let bgColor = "rgb("+redColor+","+greenColor+",0)"
-		const progressCircle =
-			<ProgressCircle
-        percent={this.state.perc}
-        radius={50}
-        borderWidth={8}
-        color="#3399FF"
-        shadowColor="#999"
-        bgColor={bgColor}>
-        <Text style={{ fontSize: 18 }}>{this.state.perc + "%"}</Text>
-      </ProgressCircle>
+const animation =
+	<AnimatedGaugeProgress
+	 size={100}
+	 width={10}
+	 fill={100}
+	 rotation={90}
+	 cropDegree={90}
+	 tintColor="#4682b4"
+	 delay={10}
+	 backgroundColor="#b0c4de"
+	 stroke={[2, 2]} //For a equaly dashed line
+	 strokeCap="circle" />
 
-		return (
-			<View>
-			 {progressCircle}
-			 {gauge}
-			 {animation}
-			 <Button title="Start" onPress={this.start}/>
-			 <Text/>
-			 <Button title="Refresh" onPress={this.onPress}/>
-			</View>)
-	}
+//sleep =
+//	ms =>
+//    new Promise(resolve => setTimeout(resolve, ms));
+//async start =
+//	() =>
+//	let i = 1
+//	while (i++<100) {
+//		await this.sleep(10)
+//		this.setState({perc: i})
+//	}
+const start =
+	() => null
+const onPress =
+	() => null
+
+const App = () => {
+  const drawer = useRef(null);
+  const [drawerPosition, setDrawerPosition] = useState("left");
+  const changeDrawerPosition = () => {
+    if (drawerPosition === "left") {
+      setDrawerPosition("right");
+    } else {
+      setDrawerPosition("left");
+    }
+  };
+
+  const navigationView = () => (
+    <View style={[styles.container, styles.navigationContainer]}>
+      <Text style={styles.paragraph}>I'm in the Drawer!</Text>
+      <Button
+        title="Close drawer"
+        onPress={() => drawer.current.closeDrawer()}
+      />
+    </View>
+  );
+
+  return (
+		<DrawerLayoutAndroid
+			ref={drawer}
+			drawerWidth={300}
+			drawerPosition={drawerPosition}
+			renderNavigationView={navigationView}>
+			<View style={styles.container}>
+				{progressCircle(90,"#8acf89")}
+				{gauge({size: 100, fill: 100, width: 10, cropDegree: 90})}
+				{animation}
+				<Button title="Start" onPress={start}/>
+				<Text/>
+				<Button title="Refresh" onPress={onPress}/>
+				<Text style={styles.paragraph}>
+				  Drawer on the {drawerPosition}!
+				</Text>
+				<Button title="Change Drawer Position" onPress={() => changeDrawerPosition()}/>
+				<Text style={styles.paragraph}>
+					Swipe from the side or press button below to see it!
+				</Text>
+				<Button title="Open drawer" onPress={() => drawer.current.openDrawer()}/>
+			</View>
+		</DrawerLayoutAndroid>
+  );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 16
+  },
+  navigationContainer: {
+    backgroundColor: "#ecf0f1"
+  },
+  paragraph: {
+    padding: 16,
+    fontSize: 15,
+    textAlign: "center"
+  }
+});
+
 export default App;
+
--- bin/start.sh
+++ bin/start.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+## Delete node_modules
+rm node_modules/* -rf
+
+## Reinstall pkgs
+npm install
+
+## Reset cache
+rm -rf /tmp/metro-cache
+
 ## Starting Metro
 npx react-native start
